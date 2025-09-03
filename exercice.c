6c6130eb77af2e80d5477cff33296827c44e57c9
hamzazirari <hamzazirari6002@gmail.com>
2025-09-03 17:12:37 +0200

    challenge tableau 4

--- exercice.c
+++ exercice.c
@@ -7,11 +7,10 @@ int main(){
     float total,totalf,red=0;
     
     
-    for(i=2;i<=n;i++){
-            red=i*2;
+    for(i=0;i<=n;i++){
             total=(n-1)*125-(125*red/100);
             totalf=total+125;
-        
+            red=red + 2;
         
     }
     printf("le prix total est%f",totalf);
